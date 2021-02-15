a0a821ac1360875f39eacc4797e4d56e044df7c4
Anthony <t9haan01@students.oamk.fi>
2021-02-15 18:14:57 +0200

    Users controller - all CRUD functions accessed here

---
+++ controllers/users.js
@@ -0,0 +1,98 @@
+const { v4: uuidv4 } = require('uuid');
+const users = require('../services/users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const jwtSecretKey = require('../json/jwtKey.json');
+const userData = require('../components/Users');
+
+exports.signup = (req, res, next) => {
+  const { email, username, name, address, phone } = req.body;
+  const password = bcrypt.hashSync(req.body.password, 6);
+  let newUser;
+  const result = users.findUserByEmail(email);
+  if (result) {
+    return res
+      .status(400)
+      .json({ msg: 'Email address already in system. Please login.'});
+  } else {
+    newUser = {
+      userId: uuidv4(),
+      email,
+      username,
+      password,
+      name,
+      address,
+      phone,
+      createDate: new Date().toISOString().slice(0, 10),
+    };
+    userData.push(newUser);
+    // TODO: REMEMBER JSON MSG AND USERDATA ONLY USED FOR DEBUGGING
+    return res.status(201).json({
+      msg: 'Registration completed.',
+      userCreated: { userId: newUser.userId },
+    });
+  }
+};
+
+exports.login = (req, res, next) => {
+  const username = req.user.username;
+  const password = req.user.password;
+  const result = users.findUserByName(username);
+  if (!result) {
+    const error = new Error('Not Authorized');
+    error.statusCode = 401;
+    throw error;
+  } else {
+    bcrypt.compare(password, result.password, (err, res) => {
+      if (err) {
+        const error = new Error('Password Incorrect');
+        error.statusCode = 401;
+        throw error;
+      }
+      if (res) {
+        return res.status(200).json({
+          msg: 'Authorization Successful',
+        });
+      }
+    });
+  }
+  const body = { id: result.userId };
+  const payload = { user: body };
+  const options = { expiresIn: '1h' };
+  const token = jwt.sign(payload, jwtSecretKey.key, options);
+  return res.status(200).json({ token: token, userId: body.id });
+};
+
+exports.updateUser = (req, res, next) => {
+  const userId = req.user.id;
+  const result = users.getUserById(userId);
+  if (!result) {
+    return res.status(403).json({ msg: 'Not Authorized.' });
+  } else {
+    const updateUser = req.body;
+    result.name = updateUser.name ? updateUser.name : result.name;
+    result.address = updateUser.address ? updateUser.address : result.address;
+    result.phone = updateUser.phone ? updateUser.phone : result.phone;
+    // TODO: REMEMBER JSON MSG AND USERDATA ONLY USED FOR DEBUGGING
+    res.status(201).json({
+      msg: 'User updated.',
+      user: result,
+      users: userData,
+    });
+  }
+};
+
+exports.deleteUser = (req, res, next) => {
+  const userId = req.user.id;
+  const result = users.getUserById(userId);
+  if (!result) {
+    return res.status(403).json({ msg: 'Not Authorized.' });
+  }
+  if (result) {
+    const index = users.getAllUsers().indexOf(result);
+    let removed = users.getAllUsers().splice(index, 1);
+    // TODO: REMEMBER JSON MSG AND USERDATA ONLY USED FOR DEBUGGING RESET STATUSCODE
+    return res.json({ msg: 'User Deleted.', users: removed });
+    // res.status(204).end();
+  }
+};
