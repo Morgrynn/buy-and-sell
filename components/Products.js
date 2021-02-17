d167180498147d6de4a5d58358e0ef7d29997884
Anthony <t9haan01@students.oamk.fi>
2021-02-18 01:55:05 +0200

    Changes to schema

--- components/Products.js
+++ components/Products.js
@@ -6,22 +6,50 @@ const productData = [
       "its working perfect in good condition the enter key don't work and there is no mouse coming with it and it can be used as tablet as well",
     city: 'Oulu',
     country: 'Finland',
-    images: [
+    "images": [
       {
-        originalname: 'mando.jpg',
+          "fieldname": "images",
+          "originalname": "mando.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "6eda04b40903e1050c472f74a42b3f90",
+          "path": "uploads\\6eda04b40903e1050c472f74a42b3f90",
+          "size": 744595
       },
       {
-        originalname: 'Morty.png',
+          "fieldname": "images",
+          "originalname": "Morty.png",
+          "encoding": "7bit",
+          "mimetype": "image/png",
+          "destination": "uploads/",
+          "filename": "cd64ede602128ee905a89f07e1b7713c",
+          "path": "uploads\\cd64ede602128ee905a89f07e1b7713c",
+          "size": 253974
       },
       {
-        originalname: 'Orangutan-Jungle-School-1.jpg',
+          "fieldname": "images",
+          "originalname": "Orangutan-Jungle-School-1.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "d7f3713e1953e2bb7a8902becb42efe7",
+          "path": "uploads\\d7f3713e1953e2bb7a8902becb42efe7",
+          "size": 70068
       },
       {
-        originalname: 'silicon-valley-jokes.jpg',
-      },
-    ],
-    price: 99.99,
-    shipping: false,
+          "fieldname": "images",
+          "originalname": "silicon-valley-jokes.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "6e169fad41ae5328376cabd1220f35b9",
+          "path": "uploads\\6e169fad41ae5328376cabd1220f35b9",
+          "size": 47610
+      }
+  ],
+    price: '99.99',
+    shipping: 'false',
     pickup: true,
     userId: 'e15c636e-9c72-4f3b-99fe-c72876c186f6',
     user: {
@@ -43,22 +71,50 @@ const productData = [
       'Chair is in very good condition. It was barely used. The chair is adjustable in height so you sit comfortably. Recommended for ages 4-12 years. Colour: blue',
     city: 'Oulu',
     country: 'Finland',
-    images: [
+    "images": [
       {
-        originalname: 'mando.jpg',
+          "fieldname": "images",
+          "originalname": "mando.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "6eda04b40903e1050c472f74a42b3f90",
+          "path": "uploads\\6eda04b40903e1050c472f74a42b3f90",
+          "size": 744595
       },
       {
-        originalname: 'Morty.png',
+          "fieldname": "images",
+          "originalname": "Morty.png",
+          "encoding": "7bit",
+          "mimetype": "image/png",
+          "destination": "uploads/",
+          "filename": "cd64ede602128ee905a89f07e1b7713c",
+          "path": "uploads\\cd64ede602128ee905a89f07e1b7713c",
+          "size": 253974
       },
       {
-        originalname: 'Orangutan-Jungle-School-1.jpg',
+          "fieldname": "images",
+          "originalname": "Orangutan-Jungle-School-1.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "d7f3713e1953e2bb7a8902becb42efe7",
+          "path": "uploads\\d7f3713e1953e2bb7a8902becb42efe7",
+          "size": 70068
       },
       {
-        originalname: 'silicon-valley-jokes.jpg',
-      },
-    ],
-    price: 20,
-    shipping: true,
+          "fieldname": "images",
+          "originalname": "silicon-valley-jokes.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "6e169fad41ae5328376cabd1220f35b9",
+          "path": "uploads\\6e169fad41ae5328376cabd1220f35b9",
+          "size": 47610
+      }
+  ],
+    price: '20',
+    shipping: 'true',
     pickup: false,
     userId: 'e310e721-f004-477b-be77-414a07b8fe8d',
     user: {
@@ -79,22 +135,50 @@ const productData = [
     description: 'perfect condition as new',
     city: 'Kerry',
     country: 'Ireland',
-    images: [
+    "images": [
       {
-        originalname: 'mando.jpg',
+          "fieldname": "images",
+          "originalname": "mando.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "6eda04b40903e1050c472f74a42b3f90",
+          "path": "uploads\\6eda04b40903e1050c472f74a42b3f90",
+          "size": 744595
       },
       {
-        originalname: 'Morty.png',
+          "fieldname": "images",
+          "originalname": "Morty.png",
+          "encoding": "7bit",
+          "mimetype": "image/png",
+          "destination": "uploads/",
+          "filename": "cd64ede602128ee905a89f07e1b7713c",
+          "path": "uploads\\cd64ede602128ee905a89f07e1b7713c",
+          "size": 253974
       },
       {
-        originalname: 'Orangutan-Jungle-School-1.jpg',
+          "fieldname": "images",
+          "originalname": "Orangutan-Jungle-School-1.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "d7f3713e1953e2bb7a8902becb42efe7",
+          "path": "uploads\\d7f3713e1953e2bb7a8902becb42efe7",
+          "size": 70068
       },
       {
-        originalname: 'silicon-valley-jokes.jpg',
-      },
-    ],
-    price: 20,
-    shipping: true,
+          "fieldname": "images",
+          "originalname": "silicon-valley-jokes.jpg",
+          "encoding": "7bit",
+          "mimetype": "image/jpeg",
+          "destination": "uploads/",
+          "filename": "6e169fad41ae5328376cabd1220f35b9",
+          "path": "uploads\\6e169fad41ae5328376cabd1220f35b9",
+          "size": 47610
+      }
+  ],
+    price: '20',
+    shipping: 'true',
     pickup: false,
     userId: 'e310e721-f004-477b-be77-414a07b8fe8d',
     user: {
