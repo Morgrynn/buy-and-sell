bf330102a75dbc350186863227177870bbddea81
Anthony <t9haan01@students.oamk.fi>
2021-02-21 13:43:45 +0200

    Updated Users controller for database connection

--- controllers/users.js
+++ controllers/users.js
@@ -1,117 +1,114 @@
 const { v4: uuidv4 } = require('uuid');
-const users = require('../services/users');
+const Users = require('../models/users');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const jwtSecretKey = require('../json/jwtKey.json');
-const userData = require('../components/Users');
 
-exports.signup = (req, res, next) => {
-  const { email, username, name, address, phone } = req.body;
-  const password = bcrypt.hashSync(req.body.password, 6);
-  let newUser;
-  const result = users.findUserByEmail(email);
-  if (result) {
-    return res
-      .status(400)
-      .json({ msg: 'Email address already in system. Please login.' });
-  } else {
-    newUser = {
+exports.registration = async (req, res, next) => {
+  try {
+    const { email, username, name, address, phone } = req.body;
+    const password = bcrypt.hashSync(req.body.password, 6);
+    const user = {
       userId: uuidv4(),
-      email,
-      username,
-      password,
-      name,
-      address,
-      phone,
+      email: email,
+      username: username,
+      password: password,
+      firstname: name.firstname,
+      lastname: name.lastname,
+      street: address.street,
+      number: address.number,
+      postcode: address.postcode,
+      city: address.city,
+      country: address.country,
+      phone: phone,
       createDate: new Date().toISOString().slice(0, 10),
     };
-    userData.push(newUser);
-    // TODO: REMEMBER JSON MSG AND USERDATA ONLY USED FOR DEBUGGING
-    return res.status(201).json({
-      msg: 'Registration completed.',
-      userCreated: { userId: newUser.userId },
-    });
-  }
-};
-
-exports.login = (req, res, next) => {
-  const username = req.user.username;
-  const password = req.user.password;
-  const result = users.findUserByName(username);
-  if (!result) {
-    const error = new Error('Not Authorized');
-    error.statusCode = 401;
-    throw error;
-  } else {
-    bcrypt.compare(password, result.password, (err, res) => {
-      if (err) {
-        const error = new Error('Password Incorrect');
-        error.statusCode = 401;
-        throw error;
-      }
-      if (res) {
-        return res.status(200).json({
-          msg: 'Authorization Successful',
-        });
-      }
-    });
-  }
-  const body = { id: result.userId };
-  const payload = { user: body };
-  const options = { expiresIn: '1h' };
-  const token = jwt.sign(payload, jwtSecretKey.key, options);
-  return res.status(200).json({ token: token, userId: body.id });
-};
-
-exports.getUser = (req, res, next) => {
-  const userId = req.user.id;
-  const result = users.getUserById(userId);
-  if (!result) {
-    return res.status(401).json({ msg: 'Not Authorized.' });
-  } else {
-    return res.status(200).json({
-      msg: 'User details',
-      user: result,
-    });
-  }
-};
-
-exports.updateUser = (req, res, next) => {
-  const userId = req.user.id;
-  const result = users.getUserById(userId);
-  if (!result) {
-    return res.status(401).json({ msg: 'Not Authorized.' });
-  } else {
-    const updateUser = req.body;
-    const newPassword = bcrypt.hashSync(updateUser.password, 6);
-    const newDate = new Date().toISOString().slice(0, 10)
-    result.username = updateUser.username ? updateUser.username : result.username;
-    result.password = newPassword ? newPassword : result.password;
-    result.name = updateUser.name ? updateUser.name : result.name;
-    result.address = updateUser.address ? updateUser.address : result.address;
-    result.phone = updateUser.phone ? updateUser.phone : result.phone;
-    result.createDate = newDate ? newDate : result.createDate;
-    // TODO: REMEMBER JSON MSG AND USERDATA ONLY USED FOR DEBUGGING
+    await Users.createNew(user);
     res.status(201).json({
-      msg: 'User updated.',
-      user: result
+      msg: 'Registration completed.',
+      userId: user.userId,
+    });
+  } catch (error) {
+    res.status(400).json({
+      reason: error,
     });
   }
 };
 
-exports.deleteUser = (req, res, next) => {
-  const userId = req.user.id;
-  const result = users.getUserById(userId);
-  if (!result) {
-    return res.status(401).json({ msg: 'Not Authorized.' });
+exports.login = async (req, res, next) => {
+  try {
+    const result = await Users.findUserByUsername(req.user.username);
+    const body = { id: result.userid };
+    const payload = { user: body };
+    const options = { expiresIn: '1h' };
+    const token = jwt.sign(payload, jwtSecretKey.key, options);
+    return res.status(200).json({ token: token, userId: body.id });
+  } catch (error) {
+    res.status(400).json({ reason: error });
   }
-  if (result) {
-    const index = users.getAllUsers().indexOf(result);
-    let removed = users.getAllUsers().splice(index, 1);
-    // users.getAllUsers().splice(index, 1);
-    // return removed;
-    // TODO: REMEMBER JSON MSG AND USERDATA ONLY USED FOR DEBUGGING RESET STATUSCODE
-    return res.status(200).json({ msg: 'User Deleted.', users: removed });
-    // return res.status(204).end();
+};
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await Users.getUserById(req.params.id);
+    delete user.password;
+    res.status(200).json({
+      msg: 'User details',
+      user: user,
+    });
+  } catch (error) {
+    res.status(401).json({
+      reason: error,
+    });
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const { username, name, address, phone } = req.body;
+    const password = bcrypt.hashSync(req.body.password, 6);
+    const result = await Users.getUserById(req.params.id);
+    if (result == false) {
+      res.status(400).json({ reason: 'UserId not found' });
+    } else {
+      await Users.update(
+        {
+          username: username,
+          password: password,
+          firstname: name.firstname,
+          lastname: name.lastname,
+          street: address.street,
+          number: address.number,
+          postcode: address.postcode,
+          city: address.city,
+          country: address.country,
+          phone: phone,
+          createDate: new Date().toISOString().slice(0, 10),
+        },
+        req.params.id
+      );
+      res.status(201).send();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      reason: error,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const result = await Users.deleteById(req.params.id);
+
+    if (result == false) {
+      res.status(404).json({ reason: 'UserId not found' });
+    } else {
+      res.status(200).send();
+    }
+  } catch (error) {
+    res.status(400).json({
+      reason: error,
+    });
   }
 };
